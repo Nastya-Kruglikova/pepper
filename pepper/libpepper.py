e61130587b6e1e93c49f5a628eacac66c798a644
Nastya Kruglikova <nastya@maytech.net>
2016-01-21 14:35:24 +0200

    force requests to use TLSv1

--- pepper/libpepper.py
+++ pepper/libpepper.py
@@ -28,41 +28,6 @@ except ImportError:
 logger = logging.getLogger('pepper')
 
 
-# from requests.adapters import HTTPAdapter
-# from requests.packages.urllib3.poolmanager import PoolManager
-
-
-# class SSLAdapter(HTTPAdapter):
-#     """"Transport adapter" that allows us to use SSLv3."""
-
-#     def __init__(self, ssl_version=None, **kwargs):
-#         self.ssl_version = ssl_version
-
-#         super(SSLAdapter, self).__init__(**kwargs)
-
-#     def init_poolmanager(self, connections, maxsize, block=False):
-#         self.poolmanager = PoolManager(num_pools=connections,
-#                                        maxsize=maxsize,
-#                                        block=block,
-#                                        ssl_version=ssl.PROTOCOL_TLSv1)
-
-#     def proxy_manager_for(self, proxy, **proxy_kwargs):
-#         # This method is called when there is a proxy.
-#         proxy_kwargs['ssl_version'] = ssl.PROTOCOL_TLSv1
-#         return super(ForceTLSV1Adapter, self).proxy_manager_for(proxy, **proxy_kwargs)
-
-
-from functools import wraps
-def sslwrap(func):
-    @wraps(func)
-    def bar(*args, **kw):
-        kw['ssl_version'] = ssl.PROTOCOL_TLSv1
-        return func(*args, **kw)
-    return bar
-
-ssl.wrap_socket = sslwrap(ssl.wrap_socket)
-
-
 class PepperException(Exception):
     pass
 
@@ -164,8 +129,8 @@ class Pepper(object):
         # Send request
         if not (self._ssl_verify):
             con = ssl.SSLContext(ssl.PROTOCOL_TLSv1)
-            #con.check_hostname = False
-            #con.verify_mode = ssl.CERT_NONE
+            con.check_hostname = False
+            con.verify_mode = ssl.CERT_NONE
             f = urlopen(req, context=con)
         else:
             f = urlopen(req)
